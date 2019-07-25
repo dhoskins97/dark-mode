b567e1704e776b81aaac46cb0f32a386bc2ecaa8
Dakota Hoskins <dakotaahoskinss@gmail.com>
2019-07-25 19:13:19 -0400

    added usedarkmode hook and implemented functionality

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,15 +1,13 @@
-import React, { useState , useEffect } from 'react';
+import { useEffect } from 'react';
 import { useLocalStorage } from './useLocalStorage.js';
 
-const useDarkMode = () => {
-    const [darkMode, setDarkMode] = useLocalStorage(darkModeEnabled, false);
-    const 
+export const useDarkMode = () => {
+    const [darkMode, setDarkMode] = useLocalStorage("darkModeEnabled", false);
 
     useEffect( () => {
         darkMode ? document.body.classList.add("dark-mode") : document.body.classList.remove("dark-mode");
-    })
+    }, [darkMode])
 
 
-
-
+    return [darkMode, setDarkMode]
 };
